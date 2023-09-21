1ddec56bcec6d37ffa56dbe7cfaf9f2b7ae006c9
Roberto Martins <rgm1301@gmail.com>
2023-09-21 09:59:50 -0300

    save

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LoadFileFromQueueModule } from './load-file-from-queue/load-file-from-queue.module';
 import { RabbitmqService } from './rabbitmq/rabbitmq.service';
 
 @Module({
-  imports: [],
+  imports: [LoadFileFromQueueModule],
   controllers: [AppController],
   providers: [AppService, RabbitmqService],
 })
