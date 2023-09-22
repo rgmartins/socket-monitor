20c5becf859cd3a95fe217e072be93a2be702e8d
Roberto Martins <rgm1301@gmail.com>
2023-09-22 13:11:56 -0300

    save

--- src/load-file-from-queue/load-file-from-queue.module.ts
+++ src/load-file-from-queue/load-file-from-queue.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { LoadFileFromQueueController } from './load-file-from-queue.controller';
 import { LoadFileFromQueueService } from './load-file-from-queue.service';
 import { RabbitmqService } from 'src/rabbitmq/rabbitmq.service';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
   controllers: [LoadFileFromQueueController],
-  providers: [LoadFileFromQueueService, RabbitmqService]
+  providers: [LoadFileFromQueueService, RabbitmqService, PrismaService]
 })
 export class LoadFileFromQueueModule {}
--- src/load-file-from-queue/load-file-from-queue.service.ts
+++ src/load-file-from-queue/load-file-from-queue.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { RabbitmqService } from './../rabbitmq/rabbitmq.service';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class LoadFileFromQueueService {
-    constructor(private readonly rabbitmqService: RabbitmqService) {
+    constructor(private readonly rabbitmqService: RabbitmqService, private readonly prisma: PrismaService) {
         console.log('inicio LoadFile')
     }
 
