26572b298d674af3fa9e1b1458abe4a459a9e729
Roberto Martins <rgm1301@gmail.com>
2023-09-24 12:59:37 -0300

    save

--- src/connections/dto/create-connection.dto.ts
+++ src/connections/dto/create-connection.dto.ts
@@ -1,3 +1,11 @@
 export class CreateConnectionDto {
+    connectionId: string;
     description: string;
+    queueName: string;
+    ip: string;
+    port: number;
+    role: string;
+    numConnections: number;
+    header: string;
+    probe: string;
 }
--- src/connections/entities/connection.entity.ts
+++ src/connections/entities/connection.entity.ts
@@ -2,5 +2,13 @@ import { Connection } from '@prisma/client'
 
 export class ConnectionEntity implements Connection {
     id: string;
+    connectionId: string;
     description: string;
+    queueName: string;
+    ip: string;
+    port: number;
+    role: string;
+    numConnections: number;
+    header: string;
+    probe: string;
 }
