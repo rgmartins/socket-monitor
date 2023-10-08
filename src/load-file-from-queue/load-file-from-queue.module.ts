7eaf7df37378f6f6c5a246e644ba8e0aaa60d029
Roberto Martins <rgm1301@gmail.com>
2023-10-08 12:33:46 -0300

    save

--- src/load-file-from-queue/load-file-from-queue.module.ts
+++ src/load-file-from-queue/load-file-from-queue.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
-import { LoadFileFromQueueController } from './load-file-from-queue.controller';
 import { LoadFileFromQueueService } from './load-file-from-queue.service';
 import { RabbitmqService } from 'src/rabbitmq/rabbitmq.service';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
   imports: [],
-  controllers: [LoadFileFromQueueController],
+  controllers: [],
   providers: [LoadFileFromQueueService, RabbitmqService, PrismaService]
 })
 export class LoadFileFromQueueModule { }
